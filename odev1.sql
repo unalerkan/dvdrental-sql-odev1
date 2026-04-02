07c388c7575d9433d8deffa28433de31fd674dbf
unalerkan <unalerknn@gmail.com>
2026-04-02 19:32:08 +0300

    Ödev 1 tamamlandı

---
+++ odev1.sql
@@ -0,0 +1,25 @@
+-- 1. Soru
+SELECT title, description
+FROM film;
+
+-- 2. Soru
+SELECT *
+FROM film
+WHERE length > 60 AND length < 75;
+
+-- 3.soru
+SELECT *
+FROM film 
+WHERE rental_rate = 0.99 
+AND (replacement_cost = 12.99 or replacement_cost = 28.99 ) ;
+
+--4.soru 
+SELECT last_name
+FROM customer
+WHERE first_name = 'Mary' ;
+
+--5.soru
+SELECT *
+FROM film
+WHERE NOT (length > 50 )
+AND NOT ( rental_rate = 2.99 OR rental_rate = 4.99 ) ;
