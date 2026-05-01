9b8625b06481d62b2baba39199f12e4d3e8b43e7
unalerkan <unalerknn@gmail.com>
2026-05-01 16:04:43 +0300

    odev12.sql

---
+++ odev12.sql
@@ -0,0 +1,17 @@
+SELECT COUNT(*) 
+FROM film 
+WHERE length > (SELECT AVG(length) FROM film);
+
+SELECT COUNT(*) 
+FROM film 
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+SELECT * 
+FROM film 
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) 
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+SELECT customer_id, COUNT(*) AS toplam_odeme 
+FROM payment 
+GROUP BY customer_id 
+ORDER BY toplam_odeme DESC;
